7a82ab48c5471d2a0c354724a3f3150589e81100
Shaohua Wen <shaohua.wen@gmail.com>
2016-12-27 22:56:43 +0800

    zookeeper names refine

--- commanderServer/initiate_zoo.sh
+++ commanderServer/initiate_zoo.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:/opt/electriccloud/electriccommander/bin
 if [ ! -f /opt/electriccloud/electriccommander/conf/zoo_ready ]; then
 
   cd /opt/electriccloud/electriccommander/conf
-  COMMANDER_ZK_CONNECTION=zoo1:2181 ../jre/bin/java -jar \
+  COMMANDER_ZK_CONNECTION=zookeeper1:2181 ../jre/bin/java -jar \
       ../server/bin/zk-config-tool-jar-with-dependencies.jar com.electriccloud.commander.cluster.ZKConfigTool \
       --databasePropertiesFile database.properties \
       --keystoreFile keystore \
