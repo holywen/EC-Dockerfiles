13feac982b8a2c2a56351296f173467017e31a12
Shaohua Wen <shaohua.wen@gmail.com>
2017-01-07 12:31:32 +0800

    use ftps

--- settings.mk
+++ settings.mk
@@ -1,8 +1,8 @@
 
 TAG ?= latest
-EFLOW_INSTALLER ?= ElectricFlow-7.0.0.111324
-EFLOW_INSTALLER_DOWNLOAD_PATH ?= ftp.electric-cloud.com/ElectricFlow/release_7.0/7.0.0.111324/linux/x86
-EFLOW_DOWNLOAD_USER ?= swen
+EFLOW_INSTALLER ?= ElectricFlow-7.1.0.113861
+EFLOW_INSTALLER_DOWNLOAD_PATH ?= electric-cloud.sharefileftp.com/products/flow/release_7.1/7.1.0.113861/linux/x86
+EFLOW_DOWNLOAD_USER ?= electric-cloud%2Fswen1%40electric-cloud.com
 EFLOW_DOWNLOAD_PASS ?= changeme
 
 COMMON_TARGETS = all build push pull cleandata cleanimages clean cleanall
