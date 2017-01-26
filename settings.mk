a33ca97effced29d136d66857c1c9d2ad9f3ec92
Shaohua Wen <shaohua.wen@gmail.com>
2017-01-26 23:19:33 +0800

    version update

--- settings.mk
+++ settings.mk
@@ -1,5 +1,5 @@
 
 TAG ?= latest
-EFLOW_INSTALLER ?= ElectricFlow-7.0.1.112906
+EFLOW_INSTALLER ?= ElectricFlow-7.1.0.113861
 
 COMMON_TARGETS = all build push pull cleandata cleanimages clean cleanall save load
